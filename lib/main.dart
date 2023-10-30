729686d30a9f04ba289395f968014cc6f75056ae
madhumanikanta <madhumanimadhumani0@gmail.com>
2023-10-30 11:47:52 +0530

    sliverappbar

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _Home_PageState extends State<Home_Page> {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(slivers: [
-      SliverAppBar(pinned:true,expandedHeight: 200,actions: [Icon(Icons.ice_skating)],
+      SliverAppBar(pinned:true,expandedHeight: 200,actions: [Icon(Icons.ice_skating),Icon(Icons.add_a_photo_sharp)],
         shape: Border(bottom: BorderSide.none),leading: Icon(Icons.menu),title: Text('welcome'),
         flexibleSpace: FlexibleSpaceBar(centerTitle:true,title:Text('welcome'),collapseMode: CollapseMode.values[0],background: Image(image: NetworkImage('https://cdn.pixabay.com/photo/2023/10/12/17/56/after-the-rain-8311416_640.jpg'),),),),
       SliverToBoxAdapter(child: Column(children: [SizedBox(height: 1000,),Text('hello')],),)
